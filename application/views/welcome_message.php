0c55c557961445e4c8f8400b0d128c82996a7353
Okulo <almaty-pro@yandex.ru>
2017-04-13 18:05:08 +0600

    c сайдбаром на главной

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,75 +8,92 @@
     <link rel="icon" href="favicon.ico">
     <title>MyGym</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/simple-sidebar.css" rel="stylesheet">
+           <link href="/css/style.css" rel="stylesheet">
   </head>
 
   <body>
-<nav class="navbar navbar-light mb-3" style="background-color: #e3f2fd;">
-   
-          <div class="navbar-toggleable-md ">
-            <ul class="nav navbar-nav text-md-center justify-content-md-between">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Рецепция</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Продажи</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Касса</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Инструктор</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Врач</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Бар</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Бутик</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="<?=base_url();?>admin">Админ</a>
-              </li>                                          
-            </ul>
-          </div>
-        </nav>
-        
-                
-    <div class="container">
-      <div class="jumbotron">
-      
-        <h1><?php
-        if (!$this->ion_auth->is_admin())
-		{
-			echo "you are not admin! <br /><br />";
-		}
-        echo '<pre>'; 
-        print_r($this->session->all_userdata());
-        echo '</pre>';
-        	# multiple groups (by name)
-		$group = array('supervisor', 'coach');
-		if (!$this->ion_auth->in_group($group, false, true)){
-			$this->session->set_flashdata('message', 'You must be a gangsta OR a hoodrat to view this page');
-   echo '<div data-form-alert-success="true" class="alert alert-form alert-danger text-xs-center">'.$this->session->flashdata("message").'</div>';
-                       
-		}
-	echo $user->first_name." ".$user->last_name;
-    echo "<br />";
-   
-?></h1>
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-<?php foreach ($this_user_groups as $group):?>
-                                <?php echo $group->description;?>,
-                            <?php endforeach?>
-        <pre>
-        <?php
-	var_dump($user);
-?></pre>
-        <p class="lead">This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page.</p>
-        <a class="btn btn-lg btn-primary" href="../../components/navbar/" role="button">View navbar docs &raquo;</a>
-      </div>
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Меню
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Simple Sidebar</h1>
+                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                   </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+
+<div id="container-floating">
+
+
+ <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">
+    <img class="edit" src="/images/menu.png">
+  </a>
+
+</div>
     </div>
 
 
@@ -87,5 +104,12 @@
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="/js/bootstrap.min.js"></script>
+        <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
   </body>
 </html>
